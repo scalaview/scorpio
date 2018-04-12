7e16b7b7cd71328610ff1b267d04ea618848af4f
scalaview <chailink100@gmail.com>
2018-04-12 22:49:38 +0800

    deploy

--- manager.py
+++ manager.py
@@ -19,5 +19,17 @@ def mine(node='http://127.0.0.1:5000'):
     coinbase_tx = util.get_coinbase_transaction(node)
     block = blockchain.Block.generate_next_block_from_remote_coinbas(coinbase_tx)
 
+
+@manager.command
+@manager.option('-po', '--port', help='port')
+def deploy(port=5000):
+    from tornado.wsgi import WSGIContainer
+    from tornado.httpserver import HTTPServer
+    from tornado.ioloop import IOLoop
+
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(port)
+    IOLoop.instance().start()
+
 if __name__ == '__main__':
     manager.run()
